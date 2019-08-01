0a2db7aa38ea722212a37c5705488e65ef0f63ef
Marlus Lopes <reiescalibur@gmail.com>
2019-07-31 21:35:41 -0300

    setup for cygwin support, based on "unsupported"

---
+++ cygwin/CygwinProcess.c
@@ -0,0 +1,33 @@
+/*
+htop - CygwinProcess.c
+(C) 2015 Hisham H. Muhammad
+Released under the GNU GPL, see the COPYING file
+in the source distribution for its full text.
+*/
+
+#include "Process.h"
+#include "CygwinProcess.h"
+#include <stdlib.h>
+
+/*{
+#include "Settings.h"
+
+#define Process_delete CygwinProcess_delete
+
+}*/
+
+Process* CygwinProcess_new(Settings* settings) {
+   Process* this = xCalloc(1, sizeof(Process));
+   Object_setClass(this, Class(Process));
+   Process_init(this, settings);
+   return this;
+}
+
+void CygwinProcess_delete(Object* cast) {
+   Process* this = (Process*) cast;
+   Object_setClass(this, Class(Process));
+   Process_done((Process*)cast);
+   // free platform-specific fields here
+   free(this);
+}
+
